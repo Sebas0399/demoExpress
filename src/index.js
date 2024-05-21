6a45de503b55cf14e3d7fdfcd582cfc8f0c9e755
antony.aguay <antony.aguay@gmail.com>
2024-05-21 02:14:15 +0000

    cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from 'cors'
+
 import usuarioRoutes from "./routes/usuarios.routes.js";
 import documentosRoutes from "./routes/documentos.routes.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors)
 app.use("/usuarios", usuarioRoutes);
 app.use("/documentos", documentosRoutes);
 
